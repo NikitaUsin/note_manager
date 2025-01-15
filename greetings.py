8e69e8cb454a002b20143b61d01fc9691bc199b7
NikitaUsin <usinnikita19@gmail.com>
2025-01-16 00:38:14 +0300

    Create greetings.py
    ето начало создание заметки

---
+++ greetings.py
@@ -0,0 +1,13 @@
+print("Hello, world!")
+
+username = input("Имя пользователя:")
+print(f"Привет, {username}!")
+
+title = input("Заголовок заметки:")
+content = input("Описание заметки:")
+print(f"title, {content}")
+
+status = input("Статус заметки")
+created_date = input("Начало день-месяц-год")
+issue_date = input("Конец день-месяц-год")
+print(f"status, {created_date}, {issue_date}")
