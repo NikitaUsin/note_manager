b1fe646ce83dcca2b3aa5ec3debfd688d615fd07
NikitaUsin <usinnikita19@gmail.com>
2025-01-17 01:58:49 +0300

    date_changer.py

--- date_changer.py
+++ date_changer.py
@@ -1,5 +1,11 @@
 print("Hello, world!")
 
+# '''19-03-2024, 19.03.24, 19:03:24, 03-19-2024 и даже 19-Mar-2024.'''
+date1 = ["'19-03-2024', '19.03.24', '19:03:24', '03-19-2024'"]
+date2 = ["'19-Mar-2024','19 марта 2024'"]
+print(date1)
+print(date2)
+
 temp_issue_date = input("Изменение даты:")
 temp_created_date = input("Дедлайн даты:")
 print(f"temp_issue_date {temp_created_date}")
