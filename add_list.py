bb5f7af05d5e702f2e48273dc144868d55aedf0c
NikitaUsin <usinnikita19@gmail.com>
2025-01-17 15:00:11 +0300

    add_list.py

--- add_list.py
+++ add_list.py
@@ -1,1 +1,13 @@
+print("Hello, world!")
 
+username = input("Введите ваше ФИО")
+title = input("Введите заголовок заметки")
+print(f"username {title}")
+
+content = input("Описание")
+status = input("Готовность задачи")
+print(f"content {status}")
+
+created_date = input("Начало ДД.ММ.ГГ:")
+issue_date = input("Завершение ДД.ММ.ГГ:")
+print(f"created_date {issue_date}")
