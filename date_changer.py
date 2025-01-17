f3c6ebc676b580f6284e4440ba6952b994a9f940
NikitaUsin <usinnikita19@gmail.com>
2025-01-17 15:01:19 +0300

    date_changer.py

--- date_changer.py
+++ date_changer.py
@@ -3,10 +3,11 @@ print("Hello, world!")
 date = "12.03.24"
 date2 = "12 марта 2024"
 date3 = "12-03-24"
-print(date[0:5])
-print(date2[0:8])
-print(date3[0:5])
+print(f"Короткий формат: date {date [0:5]}")
+print(f"Короткий формат: date2 {date2 [0:8]}")
+print(f"Короткий формат: date3 {date3 [0:5]}")
 
-temp_issue_date = input("Изменение ДД.ММ.ГГ:")
-temp_created_date = input("Дедлайн ДД.ММ.ГГ:")
+
+temp_issue_date = input("Изменение:")
+temp_created_date = input("Дедлайн :")
 print(f"temp_issue_date {temp_created_date}")
