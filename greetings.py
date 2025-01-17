b81c020af837617be93777c1c4be65f799273708
NikitaUsin <usinnikita19@gmail.com>
2025-01-18 02:13:22 +0300

    greetings.py

--- greetings.py
+++ greetings.py
@@ -1,13 +1,13 @@
 print("Hello, world!")
 
 username = input("Имя пользователя:")
-print(f"Привет, {username}!")
+print(f"Привет, {username}!")                    # Делает приветствие и вовод имени пользователем
 
 title = input("Заголовок заметки:")
 content = input("Описание заметки:")
-print(f"title, {content}")
+print(f"title, {content}")                       # Задаем заголовок и описание
 
 status = input("Статус заметки")
 created_date = input("Начало день-месяц-год")
 issue_date = input("Конец день-месяц-год")
-print(f"status, {created_date}, {issue_date}")
+print(f"status, {created_date}, {issue_date}")    # Задем статус, дату начала и конец
