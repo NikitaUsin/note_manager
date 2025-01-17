273cb6000fe3765dde65e89ae3b6c401ca7f2b0c
NikitaUsin <usinnikita19@gmail.com>
2025-01-18 02:13:55 +0300

    date_changer.py

--- date_changer.py
+++ date_changer.py
@@ -5,9 +5,8 @@ date2 = "12 марта 2024"
 date3 = "12-03-24"
 print(f"Короткий формат: date {date [0:5]}")
 print(f"Короткий формат: date2 {date2 [0:8]}")
-print(f"Короткий формат: date3 {date3 [0:5]}")
-
+print(f"Короткий формат: date3 {date3 [0:5]}")     # Делаем короткий формат по датам
 
 temp_issue_date = input("Изменение:")
 temp_created_date = input("Дедлайн :")
-print(f"temp_issue_date {temp_created_date}")
+print(f"temp_issue_date {temp_created_date}")      # Задаем изменение дедлайна даты
