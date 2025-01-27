70efc5e689ec15ad2726eb9df42dda4ada464a9a
NikitaUsin <usinnikita19@gmail.com>
2025-01-27 20:19:26 +0300

    final.py

--- final.py
+++ final.py
@@ -1,22 +1,20 @@
 print("Hello, world!")
 
-struct = (["username", "Описание заметки:", "Статус заметки", "Начало день-месяц-год", "Завершение ДД.ММ.ГГ:",
-           ["Заголовок 1", "Заголовок 2"]])  # вложенный список для заголовков
-print(struct[0])                             # Имя пользователя
-print(struct[1])                             # Содержание заметки
-print(struct[2])                             # Статус
-print(struct[3])                             # Дата создания
-print(struct[4])                             # Дата изменения
-print(struct[5])                             # ['Заголовок 1', 'Заголовок 2']
+username = input("Введите ваше ФИО")
+content = input("Описание")
+status = input("Готовность задачи")
+created_date = input("Начало ДД.ММ.ГГ:")
+issue_date = input("Завершение ДД.ММ.ГГ:")
+temp_issue_date = input("Изменение :")
+temp_created_date = input("Дедлайн :")
 
-struct = input(struct[0])                    # Обращение к элементам списка
-print(f"Привет,{struct[1]}!")
+struct = ([username, content, status, created_date, issue_date,
+           [temp_issue_date, temp_created_date]])  # вложенный список для заголовков
 
-struct = input(struct[2])
-print(f"{struct[3]}")
-
-struct = input(struct[3])
-print(f"{struct[4]}")
-
-struct = input(struct[4])
-print(f"{struct[5]}")
+print(f"Введите ваше ФИО: struct [0]")
+print(f"Описание: struct [1]")
+print(f"Готовность задачи: struct [2]")
+print(f"Начало ДД.ММ.ГГ: struct [3]")
+print(f"Завершение ДД.ММ.ГГ: struct [4]")
+print(f"Изменение : struct [5]")
+print(f"Дедлайн : struct [6]")
